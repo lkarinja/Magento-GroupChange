bdbf1a8ffec58752abb40056ee045fcd16a03336
LK <leejaepublic@gmail.com>
2017-06-30 13:43:49 -0400

    Update InstallSchema.php
    Added Copyright Notice

--- Setup/InstallSchema.php
+++ Setup/InstallSchema.php
@@ -1,5 +1,22 @@
 <?php
+/*
+Copyright © 2017 Leejae Karinja
 
+This file is part of GroupChange.
+
+GroupChange is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+GroupChange is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GroupChange.  If not, see <http://www.gnu.org/licenses/>.
+*/
 namespace PlymDesign\GroupChange\Setup;
 
 use Psr\Log\LoggerInterface;
